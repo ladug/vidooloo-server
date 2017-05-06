7cd9e79f62689143b1de92a9665a1f2e4ed13cac
Vladimir Volchkov <admin@howtoclient.com>
2017-05-06 16:32:57 +0200

    initial nodejs express server

---
+++ routes.js
@@ -0,0 +1,14 @@
+/**
+ * Created by vladi on 05-May-17.
+ */
+module.exports = [
+    /*{
+        method: "get",
+        url: '/api/getUserData',
+        handlers: [
+            (req, res) => {
+                res.send(200, req.user);
+            }
+        ]
+    }*/
+];
