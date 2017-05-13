d96a6af4d5d98d398c52b47f965235977b43bff3
Vladimir Volchkov <admin@howtoclient.com>
2017-05-13 23:57:40 +0200

    minor fix

--- services/mp4-analizer/test.js
+++ services/mp4-analizer/test.js
@@ -13,7 +13,7 @@ const convertHeadersForComparison = (mp4) => {
     },
     createBinaryString = (nMask) => {
         // nMask must be between -2147483648 and 2147483647
-        for (var nFlag = 0, nShifted = nMask, sMask = ''; nFlag < 32;
+        for (let nFlag = 0, nShifted = nMask, sMask = ''; nFlag < 32;
              nFlag++, sMask += String(nShifted >>> 31), nShifted <<= 1);
         return sMask.slice(-24);
     };
