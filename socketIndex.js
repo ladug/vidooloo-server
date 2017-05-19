4c42756c01d83bca21d500da3e24e47954a6738e
volodya <vladimir.tolubayev@mail.huji.ac.il>
2017-05-19 17:16:51 +0300

    working with ws library

--- socketIndex.js
+++ socketIndex.js
@@ -1,19 +1,11 @@
-/**
- * Created by volodya on 5/19/2017.
- */
-const net = require('net');
-const server = net.createServer((c) => {
-    // 'connection' listener
-    console.log('client connected');
-    c.on('end', () => {
-        console.log('client disconnected');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3101 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
     });
-    c.write('vodioolo socket server reached\r\n');
-    c.pipe(c);
-});
-server.on('error', (err) => {
-    throw err;
-});
-server.listen(3101, () => {
-    console.log('socket server @ http://localhost:3101');
+
+    ws.send('something');
 });
