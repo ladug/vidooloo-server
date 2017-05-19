c7a8ebcea65033dae735d88453b1b64a8cd0cbaf
volodya <vladimir.tolubayev@mail.huji.ac.il>
2017-05-19 22:18:14 +0300

    basic test for returning array

--- socketIndex.js
+++ socketIndex.js
@@ -7,9 +7,28 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         const messageObj = JSON.parse(message);
         console.log('received: %s', message);
+
+        //test object communication
+       // messageObj.testProp = 'vidooloo';
+       // ws.send(JSON.stringify(messageObj));
+
+
+        const array = new Uint8Array(5);
+
+        for (var i = 0; i < array.length; ++i) {
+            array[i] = i ;
+        }
+
+        ws.send(array);
+
+        for (var i = 0; i < array.length; ++i) {
+            array[i] = i + 10 ;
+        }
+
+        ws.send(array);
     });
 
-    ws.send('vidooloo');
+
 });
 
 
