4c56269d139426b5e6e97c3368a62741fdba66bd
volodya <vladimir.tolubayev@mail.huji.ac.il>
2017-05-19 19:50:49 +0300

    last commit before branch close operations. includes small test for sockets

--- socketIndex.js
+++ socketIndex.js
@@ -1,11 +1,14 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3101 });
+console.log('SocketServer listening to localhost:3101');
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
 
-    ws.send('something');
+    ws.send('vidooloo');
 });
+
+
